85b77594d55437bc3b381a06b7f87aece686669e
fedya1909 <fedor.vlasov.2000@gmail.com>
2021-05-17 03:02:49 +0300

    UpdateTG
    I add fitch for checking users in database

--- basedata.py
+++ basedata.py
@@ -2,22 +2,24 @@ import sqlite3
 
 class SqlLiteDB:
 
+
     def __init__(self, database_file):
-        self.connection = sqlite3.connect(database_file)
+        self.connection = sqlite3.connect(database_file, check_same_thread = False)
         self.cursor = self.connection.cursor()
     
-    def add_users(self, id, user_id, username, firstname):
+    def create_table(self):
+        return self.cursor.execute('CREATE TABLE IF NOT EXISTS users (userID integer primary key)')
+        
+    def add_users(self, user_id):
+        self.create_table()
         with self.connection:
-            self.cursor.execute(f"INSERT INTO users VALUES (?,?,?,?)",(id, user_id, username, firstname))
+            return self.cursor.execute("INSERT OR REPLACE INTO 'users' VALUES (?)",(user_id,))
     
     def checking_users(self, user_id):
+        self.create_table()
         with self.connection:
-            self.cursor.execute("SELECT user_id FROM users WHERE user_id = ?",(user_id))
-            result = self.cursor.fetchone()
-            if result is None:
-                return True
-            else:
-                return False
+            result = self.cursor.execute("SELECT 'userID' FROM 'users' WHERE 'userID' = ?",(user_id,)).fetchall()
+            return bool(len(result))
     
     def close(self):
         self.connection.close()
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-import telebot
+import telebot as tg
 import config
 from basedata import SqlLiteDB
 
 token = config.TOKEN
 admin_id = ''
 
-bot = telebot.TeleBot(token)
+bot = tg.TeleBot(token)
 db = SqlLiteDB('db.db')
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(content_types=['text'])
 def start(message):
-    db.add_users(message.chat.id ,message.chat.id, message.chat.username, message.chat.first_name)
-    bot.send_message(message.chat.id, f"Добро пожаловать, {message.chat.username}")
-    db.close()
-
+    if message.text.strip().lower() == '/start':
+        if not db.checking_users(message.chat.id):
+            db.add_users(message.from_user.id)
+            db.connection.commit()
+            return bot.send_message(message.chat.id, f"Добро пожаловать {message.from_user.first_name}!\n"
+                                                     f"Ваш никнейм: {message.from_user.username}\n"
+                                                     f"Ваш ID: {message.from_user.id}")
+        else:
+            return bot.send_message(message.chat.id, f"ОООО, Ты снова здесь, {message.from_user.first_name}!\n"
+                                                     f"Ваш никнейм: {message.from_user.username}\n"
+                                                     f"Ваш ID: {message.from_user.id}")
 
 bot.polling(none_stop=True)
